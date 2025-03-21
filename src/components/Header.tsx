cd888c26d6eeb89098972ab95ad0e7c0e4c27aaa
aslynclmrzn <ashnicolemarzan@gmail.com>
2025-03-21 18:12:59 +0800

    feat(header): add the top header

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import './Header.scss'
+import { Avatar } from '@mui/material';
+function Header() {
+  return (
+  
+    <>
+    <div className='header-container'> 
+    <div className="left-content">
+        <Avatar alt="d1c3cipher" src="/static/images/avatar/1.jpg" className='avatar' /> 
+        <p> Dicecipher </p>
+    </div>
+        <div className='box-container'>
+        <span className='box1'></span>
+        <span className='box2'></span>
+        <span className='box3'></span>
+        <span className='box4'></span>
+        <span className='box5'></span>
+        </div>
+        
+    </div>
+    </>
+    
+  )
+}
+
+export default Header
