50b8902ce8ecb8f10f0a169a1bb4786bb4c4e763
aslynclmrzn <ashnicolemarzan@gmail.com>
2025-03-29 13:52:02 +0800

    feat(projects): add new projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -17,11 +17,27 @@ function Projects() {
           </div>
           <div className='bottom-bar'></div>
         </div>
+
         <div className='projects-container'>
         <div className='projects-content'>
-          <h1 className='project-name'>Tamix<span className='year'>(2023)</span></h1>
-          <Chip className="tools"  label="WordPress"/>
-          <Chip className="tools"  label="WordPress"/>
+          <h1 className='project-name'>UnsentWords<span className='year'>(2025)</span></h1>
+          <Chip className="tools"  label="HTML"/>
+          <Chip className="tools"  label="CSS"/>
+          <Chip className="tools"  label="JS"/>
+          <Tooltip title="Open Link" arrow>
+          <a className="circle-icon" href="https://tamix.com/" target="_blank" rel="noopener noreferrer">
+            <CallMadeOutlinedIcon className="icon" />
+          </a>
+        </Tooltip>
+        </div>
+        <div className='bottom-bar'></div>
+      </div>
+
+      <div className='projects-container'>
+        <div className='projects-content'>
+          <h1 className='project-name'>Retrato<span className='year'>(2025)</span></h1>
+          <Chip className="tools"  label="NextJS"/>
+          <Chip className="tools"  label="Tailwind"/>
           <Tooltip title="Open Link" arrow>
           <a className="circle-icon" href="https://tamix.com/" target="_blank" rel="noopener noreferrer">
             <CallMadeOutlinedIcon className="icon" />
