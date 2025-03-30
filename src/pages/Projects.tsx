bc5ba4c94b53997b0d26c679c6e473c8f2112460
aslynclmrzn <ashnicolemarzan@gmail.com>
2025-03-31 00:01:11 +0800

    feat(projects): add project link and chips

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -20,12 +20,13 @@ function Projects() {
 
         <div className='projects-container'>
         <div className='projects-content'>
-          <h1 className='project-name'>UnsentWords<span className='year'>(2025)</span></h1>
+          <h1 className='project-name'>Unsent Words<span className='year'>(2025)</span></h1>
           <Chip className="tools"  label="HTML"/>
-          <Chip className="tools"  label="CSS"/>
-          <Chip className="tools"  label="JS"/>
+          <Chip className="tools"  label="Bootstrap"/>
+          <Chip className="tools"  label="JavaScript"/>
+          <Chip className="tools"  label="Firebase"/>
           <Tooltip title="Open Link" arrow>
-          <a className="circle-icon" href="https://tamix.com/" target="_blank" rel="noopener noreferrer">
+          <a className="circle-icon" href="https://unsentwords-db234.web.app/" target="_blank" rel="noopener noreferrer">
             <CallMadeOutlinedIcon className="icon" />
           </a>
         </Tooltip>
